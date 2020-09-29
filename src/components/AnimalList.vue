ddc59fed753ad65b157382b3f47369fe06a684d4
ivanamilovac <antesevicivana@gmail.com>
2020-09-29 19:02:12 +0200

    added animals to animal list and printed table with animal data

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,41 +1,71 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>sort</th>
+          <th>name</th>
+          <th>date of birth</th>
+        </tr>
+
+      </thead>
+      <tbody>
+        <tr v-for="animal in animals" :key="animal">
+          <td> {{ animal.sort}}</td>
+          <td> {{ animal.name}}</td>
+          <td>{{ animal.dateOfBirth}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'AnimalList',
-  props: {
-    msg: String
+  data() {
+    return {
+      animals: [
+      
+          {
+            sort: 'pas',
+            name: 'kerina',
+            dateOfBirth: new Date(2020, 7, 15).toLocaleString()
+          },
+          {
+            sort: 'macka',
+            name: 'mica',
+            dateOfBirth: new Date(2019, 8, 19).toLocaleString()
+          },
+          {
+            sort: 'krava',
+            name: 'kravica',
+            dateOfBirth: new Date(2018, 1, 11).toLocaleString()
+          },
+          {
+            sort: 'svinja',
+            name: 'dlakava',
+            dateOfBirth: new Date(2017, 2, 5).toLocaleString()
+          },
+          {
+            sort: 'majmun',
+            name: 'majmunko',
+            dateOfBirth: new Date(2016, 4, 23).toLocaleString()
+          }
+    
+      
+      ]
+    }
   }
+  
+    // cat
+    // cow
+    // pig
+    // monkey
+
+
 }
 </script>
 
@@ -54,5 +84,21 @@ li {
 }
 a {
   color: #42b983;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 400px;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
 }
 </style>
